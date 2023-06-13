89544aab7bd768f765726dfd9f4e3ee79fee92f7
sonnny <sonnybalut@yahoo.com>
2023-06-13 15:05:03 -0700

    Update counter.js

--- apps/counter/counter.js
+++ apps/counter/counter.js
@@ -16,42 +16,43 @@ function connect(){
 
 function init() {
 Bangle.on("drag", e => {
-		if (!drag) { // start dragging
+		if (!drag) {
 			drag = {x: e.x, y: e.y};
-		} else if (!e.b) { // released
+		} else if (!e.b) {
 			const dx = e.x-drag.x, dy = e.y-drag.y;
 			drag = null;
 			if (Math.abs(dx)>Math.abs(dy)+10) {
-				// horizontal
+
 				if (dx < dy) {
-					tx.writeValue(0x6c);
-					//sendText("left");
-				} else {
 					tx.writeValue(0x72);
-					//sendText("right");
+
+				} else {
+					tx.writeValue(0x6c);
+
 				}
 			} else if (Math.abs(dy)>Math.abs(dx)+10) {
-				 //vertical
 				if (dx < dy) {
 					tx.writeValue(0x66);
-					//sendText("down");
+
 				} else {
 					tx.writeValue(0x62);
-					//sendText("up");
+	
 				}
 			} else {
-          if (e.x > 140 && e.y > 140) {tx.writeValue(0x73);}//lower right
-			    else if (e.x < 45 && e.y < 45)   {console.log(" "+e.x+" "+e.y);}
-			    else if (e.x > 140 && e.y < 45)  {console.log(" "+e.x+" "+e.y);}
-			    else if (e.x < 45 && e.y > 140)  {connect();}//lower right
-      }
-			
-		} // else released
+          if (e.x > 140 && e.y > 140) {tx.writeValue(0x73);}
+			    else if (e.x < 45 && e.y < 45)   {}
+			    else if (e.x > 140 && e.y < 45)  {}
+			    else if (e.x < 45 && e.y > 140)  {connect();}
+      }			
+		}
 });
 }
 g.clear();
 g.setColor(0,0,0);
-g.drawString("start swiping...",50,50);
+g.setFont("6x8:2");
+g.drawString("lower right",1,1);
+g.drawString("connect",1,20);
+g.drawString("start swipe",1,40);
 g.setColor(1,0,0);
 g.fillCircle(145,145,10);
 init();
